51335b1b054e6f09e840aa6ae944d2df4b5bb7b6
Snow Camama <reham.snow29@gmail.com>
2018-03-24 15:38:14 +0800

    3.24.18

---
+++ advweb/app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :login
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable,
+         :validatable, authentication_keys: [:login]
+
+  validates :age, presence: true, numericality: { only_integer: true }
+  validates :age, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).ex ists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+end
---
+++ advweb/db/migrate/20180324064941_add_profile_to_users.rb
@@ -0,0 +1,9 @@
+class AddProfileToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :age, :integer
+    add_column :users, :username, :string
+    add_index :users, :username, unique: true
+  end
+end
